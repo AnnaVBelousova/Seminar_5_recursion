20fdb3d373c32883852bfd14275a2912af4a6642
AnnaVBelousova <119129773+AnnaVBelousova@users.noreply.github.com>
2023-04-06 23:08:57 +0500

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,25 +1,3 @@
 
 
 
-def sum_of_element( number, count=0 ):
-      
-    if  number!=0:
-        
-        if (number-1)%2==0:
-            count = count+(1/2**(number-1))
-            #print (1/2**(number-1))
-        else: 
-            count = count+(1/2**(number-1))*(-1)
-            #print (count)
-        return sum_of_element( number-1, count)
-       
-    else: 
-        if number == 0:
-            return count
-        
-try:
-    number = int(input("Введите число:")) 
-    res = sum_of_element(number, count = 0)
-    print(res)
-except ValueError:
-    print("Это не число!")
