410fc2e5e0aa13b92ee47de3c66041e9dd62b773
AnnaVBelousova <119129773+AnnaVBelousova@users.noreply.github.com>
2023-04-06 23:08:44 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,22 +2,3 @@
 
 
 
-def invert_number(N, lst = []):
-
-      last = N%10
-      lst.append(last)
-      invert_number_as_string = ''.join(map(str,lst))
-      invert_number_as_string.replace(' ','')
-      
-      if  N//10==0:
-          return invert_number_as_string
-      
-      return  invert_number(N//10, lst)
-
-try:
-    N = int(input("Введите число:"))
-    res = invert_number(N, lst = [])
-    print(res)
-    
-except ValueError:
-    print("Это не число!")
