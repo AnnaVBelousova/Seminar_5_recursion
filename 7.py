323863c12f153e64478d44936da459ac24eb08a9
AnnaVBelousova <119129773+AnnaVBelousova@users.noreply.github.com>
2023-04-06 23:09:27 +0500

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,25 +1,3 @@
 
 
 
-
-def summa_of_numbers(n, lst = []):
-
-    if n-1 == -1:
-        return sum(lst)
-    else:
-        lst.append(n)
-    return summa_of_numbers(n-1, lst) 
-
-try:
-    n = int(input("Введите число:"))
-    res = summa_of_numbers(n, lst = [])
-    print (res)
-
-    checkout = n*(n+1)/2
-
-    if  checkout == res:
-        print("Равенство выполняетcя")
-    else :
-        print("Равенство не выполняется")
-except ValueError:
-    print ("Это не число!")
