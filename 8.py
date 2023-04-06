a1683005f08faf0e6bbefe824b5102e22e875506
AnnaVBelousova <119129773+AnnaVBelousova@users.noreply.github.com>
2023-04-06 23:09:39 +0500

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,17 +1,3 @@
 
 
-def power(A,B):
-    if B == 1:
-        return A
-    else:         
-        return A *power (A,B-1)
-    
-try:
-    A = int(input("Введите число:"))
-    B = int(input("Введите число:"))
-   
-    res = power(A,B)
-    print(res)
 
-except ValueError:
-    print ("Это не число!")
